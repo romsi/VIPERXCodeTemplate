a6bbccf50b5849d1840a9ad7ecb5705b417dfe81
Romain Asnar <romain.asnar@gmail.com>
2015-07-05 18:47:21 +0200

    Add error handling and replace UIKit importation by Foundation.

--- Architecture/VIPER.xctemplate/___FILEBASENAME___DataManager.swift
+++ Architecture/VIPER.xctemplate/___FILEBASENAME___DataManager.swift
@@ -6,8 +6,7 @@
 //___COPYRIGHT___
 //
 
-import UIKit
-import CoreData
+import Foundation
 
 class ___FILEBASENAMEASIDENTIFIER___DataManager
 {
@@ -16,4 +15,47 @@ class ___FILEBASENAMEASIDENTIFIER___DataManager
     // MARK: - Life cycle
 
     // MARK: - Data management
+    
+    // MARK: - Error
+    
+    enum ___FILEBASENAMEASIDENTIFIER___DataManagerError
+    {
+        case Mapping(String)
+        
+        var code: Int {
+            switch self {
+            case .Mapping:
+                return 500
+            }
+        }
+        
+        var domain: String {
+            switch self {
+            case .Mapping:
+                return "MappingDomain"
+            }
+        }
+        
+        var description: String {
+            switch self {
+            case .Mapping:
+                return "Mapping Error."
+            }
+        }
+        
+        var reason: String {
+            switch self {
+            case .Mapping(let JSONString):
+                return "Response string can not be mapped to the object.\nString: \(JSONString)."
+            }
+        }
+        
+        var error: NSError {
+            let userInfo = [
+                NSLocalizedDescriptionKey: self.description,
+                NSLocalizedFailureReasonErrorKey: self.reason
+            ]
+            return NSError(domain: self.domain, code: self.code, userInfo: userInfo)
+        }
+    }
 }
